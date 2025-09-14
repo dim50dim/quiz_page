95a23f2a39f8efafe47ac13c6ac2b1c8bc05b033
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-14 19:37:48 +0200

    as always removed template of next js.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center text-4xl text-blue-400'>
+        About page
+    </div>
+  )
+}
+
+export default page;
---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div>Quiz page</div>
+    </div>
+  )
+}
+
+export default page
+  
