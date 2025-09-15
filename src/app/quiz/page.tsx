dc2ead2b17418ed15e331ab44443a987c88fdcd2
¨dim50dim¨ <zheliezkov@gmail.com>
2025-09-15 08:00:55 +0200

    created one more function onAnswerSelected with two parameters.

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import {quiz} from '../data.js'
 
+
 const page = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState('');
@@ -14,16 +15,38 @@ const page = () => {
     wrongAnswers:0,
   });
    const {questions} = quiz;
-   const [question, answers,correctAnswer] = questions[activeQuestion];
+   const {question, answers,correctAnswer} = questions[activeQuestion];
+
+   const onAnswerSelected  = ((answer,index)) => {
+      setChecked(true);
+      setSelectedAnswerIndex(index)
+   }
 
   return (
     <div className='container'>
-         <h1>Quiz Page</h1>
+         <h1 className='text-2xl font-bold '>Quiz Page</h1>
          <div>
             <h2>Question : {activeQuestion + 1}
                          <span>/{questions.length}</span>
             </h2>
    
+         </div>
+         <div>
+            {!showResult ? (
+            <div className='quiz-container'>
+                <h3>{questions[activeQuestion].question}</h3>
+                {answers.map((answer,index)=> {
+                    <li key={index} 
+                    onClick={() => onAnswerSelected(answer,index)}
+                    className={setSelectedAnswerIndex === index ? 'li-selected' :'li-hover'}>
+                        <span>{answer}</span>
+                    </li>
+                })}
+            </div>
+            
+            ) : (
+            <div className='quiz-container'></div>
+            )}
          </div>
     </div>
   )
